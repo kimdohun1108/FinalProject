2ac8187bee12a5c2737286e87e54d923a5842933
kimdohun1108 <talkes23@naver.com>
2024-07-25 11:07:34 +0900

    3

---
+++ src/main/reactjs/src/App.js
@@ -0,0 +1,16 @@
+import img1 from './image_readmed_2021_1129972_16392006804880493.jpg';
+
+function App() {
+  return (
+    <div>
+      <img alt="main"
+         src={img1}
+         style={{width:'300px',border:'5px inset gray'}}/>
+      <h3>
+        <b>스프링부트+리액트 프로젝트 배포 성공기원!!!</b>
+      </h3>
+    </div>
+  );
+}
+
+export default App;

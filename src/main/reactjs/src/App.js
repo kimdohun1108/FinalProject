274b4ba839230bfea408fef3dec6e49ef240a4e1
kimdohun1108 <talkes23@naver.com>
2024-08-21 19:45:06 +0900

    qwer

--- src/main/reactjs/src/App.js
+++ src/main/reactjs/src/App.js
@@ -21,9 +21,10 @@ configureUrls();
 function configureUrls() {
     // 로컬 개발을 위한 URL 구성
     // 프로덕션을 위한 URL 구성
-    APPLICATION_SERVER_URL = "http://223.130.139.215:6080/";
+    APPLICATION_SERVER_URL = "http://223.130.139.215:6443/";
     LIVEKIT_URL = "wss://openvidu.quizverse.kro.kr/";
 }
+// 로컬이면 6080, 배포면 6443
 
 // function configureUrls() {
 //     // If APPLICATION_SERVER_URL is not configured, use default value from OpenVidu Local deployment

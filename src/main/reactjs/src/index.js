b2ecf69e417b4a8d5de0685abe2fda423b0fe276
kimdohun1108 <talkes23@naver.com>
2024-08-23 11:01:09 +0900

    zxc

--- src/main/reactjs/src/index.js
+++ src/main/reactjs/src/index.js
@@ -8,7 +8,7 @@ import App22 from './App22';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App22 />
+    <App />
   </React.StrictMode>
 );
 

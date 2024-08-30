d3e0c47bb09574abf6d4d66ec670459bf813b2c3
kimdohun1108 <talkes23@naver.com>
2024-08-30 09:56:58 +0900

    주

--- src/main/reactjs/src/App3.js
+++ src/main/reactjs/src/App3.js
@@ -264,47 +264,48 @@ function App() {
 
     //채팅
     const [message, setMessage] = useState('');
-  const [messages, setMessages] = useState([]);
-  const [socket, setSocket] = useState(null);
+    const [messages, setMessages] = useState([]);
+    const [socket, setSocket] = useState(null);
 
-  useEffect(() => {
-    const ws = new WebSocket('wss://openvidu.quizver.kro.kr/ws/chat');
+    //wss://openvidu.quizver.kro.kr/ws/chat
+    useEffect(() => {
+        const ws = new WebSocket('wss://openvidu.quizver.kro.kr');
+        
+        ws.onopen = () => {
+        console.log('웹소켓 연결이 설정되었습니다.');
+        };
+
+        ws.onmessage = (event) => {
+        console.log('메시지 수신됨:', event.data);
+        setMessages((prevMessages) => [...prevMessages, `${participantName} : ${event.data}`]);
+        };
     
-    ws.onopen = () => {
-      console.log('웹소켓 연결이 설정되었습니다.');
-    };
-
-    ws.onmessage = (event) => {
-      console.log('메시지 수신됨:', event.data);
-      setMessages((prevMessages) => [...prevMessages, `${participantName} : ${event.data}`]);
-    };
-  
-    ws.onclose = () => {
-      console.log('웹소켓 연결이 종료되었습니다.');
-    };
-  
-    ws.onerror = (error) => {
-      console.error('웹소켓 오류 발생:', error);
-    };
-
-    setSocket(ws);
+        ws.onclose = () => {
+        console.log('웹소켓 연결이 종료되었습니다.');
+        };
     
-    return () => {
-        ws.close();
-    };
-    }, [participantName]);
+        ws.onerror = (error) => {
+        console.error('웹소켓 오류 발생:', error);
+        };
 
-  const sendMessage = (e) => {
-    e.preventDefault();
-    if (socket && message) {
-      socket.send(message);
-      //setMessages((prevMessages) => [...prevMessages, `나: ${message}`]); // 나의 메시지를 화면에 추가
-      setMessage(''); // 메시지 입력란 비우기
-    }
-    else {
-      console.warn('소켓이 열려 있지 않거나 메시지가 비어 있습니다.');
-    }
-  };
+        setSocket(ws);
+        
+        return () => {
+            ws.close();
+        };
+        }, [participantName]);
+
+    const sendMessage = (e) => {
+        e.preventDefault();
+        if (socket && message) {
+        socket.send(message);
+        //setMessages((prevMessages) => [...prevMessages, `나: ${message}`]); // 나의 메시지를 화면에 추가
+        setMessage(''); // 메시지 입력란 비우기
+        }
+        else {
+        console.warn('소켓이 열려 있지 않거나 메시지가 비어 있습니다.');
+        }
+    };
 
     return (
         <LayoutContextProvider>

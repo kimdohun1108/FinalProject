4f0e708ce9efa4a1e2892192f08d4bce76b0892f
kimdohun1108 <talkes23@naver.com>
2024-08-30 10:24:59 +0900

    주

--- src/main/reactjs/src/App3.js
+++ src/main/reactjs/src/App3.js
@@ -267,9 +267,9 @@ function App() {
     const [messages, setMessages] = useState([]);
     const [socket, setSocket] = useState(null);
 
-    // wss://localhost:5080/ws/chat
+    // wss://localhost:6080/ws/chat
     useEffect(() => {
-        const ws = new WebSocket('wss://openvidu.quizver.kro.kr/ws/chat');
+        const ws = new WebSocket('wss://openvidu.quizver.kro.kr:6080/ws/chat');
         
         ws.onopen = () => {
         console.log('웹소켓 연결이 설정되었습니다.');

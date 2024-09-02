8fe0bb6af0913f6ebe1b5220874b2b2834bb017f
kimdohun1108 <talkes23@naver.com>
2024-09-02 16:05:30 +0900

    asd

--- src/main/java/com/example/demo/WebsocketConfig.java
+++ src/main/java/com/example/demo/WebsocketConfig.java
@@ -15,8 +15,8 @@ public class WebsocketConfig implements WebMvcConfigurer, WebSocketConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**") // 모든 엔드포인트에서 CORS 설정
-				.allowedOrigins("http://localhost:3000")
-				//.allowedOrigins("http://223.130.139.215:3000", "https://openvidu.quizver.kro.kr") //react 주소
+				//.allowedOrigins("http://localhost:3000")
+				.allowedOrigins("http://223.130.139.215:3000", "https://openvidu.quizver.kro.kr") //react 주소
 				.allowedMethods("GET","POST","PUT","DELETE","OPTIONS")// 허용할 HTTP 메서드
 				.allowedHeaders("*") //적용할 헤더
 				.allowCredentials(true);
@@ -25,8 +25,8 @@ public class WebsocketConfig implements WebMvcConfigurer, WebSocketConfigurer {
 	 @Override
 	 public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
 		 registry.addHandler(new ChatWebSocketHandler(), "/ws/chat")
-	             .setAllowedOrigins("http://localhost:3000") // React 앱 주소
-	             //.setAllowedOrigins("https://openvidu.quizver.kro.kr")
+	             //.setAllowedOrigins("http://localhost:3000") // React 앱 주소
+	             .setAllowedOrigins("https://openvidu.quizver.kro.kr")
 	             .addInterceptors(new HttpSessionHandshakeInterceptor());
 	 }	
 }
